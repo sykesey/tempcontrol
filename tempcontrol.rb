51b935f04979f63992e23ba3daa72e0dfaa525a2
Ben Sykes <me@sykesey.id.au>
2011-06-30 21:18:51 +1000

    Add gnuplot

--- tempcontrol.rb
+++ tempcontrol.rb
@@ -14,16 +14,23 @@ def get_temperatures
           if opts["type"] == "sensor" 
 		  probe_temp = read_device_temperature id
 		  puts "#{opts["name"]}: #{ read_device_type id } #{ probe_temp }"
-		  temp = Temperature.new( "probe_id" => id, 
-					  "probe_name" => opts["name"],
-					  "value" => probe_temp )
-                  temp.save
+                  save_temp id, opts["name"], probe_temp
           end
 	end
 end
 
+def save_temp (id,name,value)
+    temp = Temperature.new( "probe_id" => id,
+                            "probe_name" => name,
+                            "value" => value )
+    temp.save
+end
+
 def main
-  get_temperatures
+  while true
+   get_temperatures
+   sleep 60
+  end
 end
 
 main
